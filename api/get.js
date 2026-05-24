5e7243a5c1d0c93a1c6f286167f906f33f271fc5
[dyad] <omarhrytani@gmail.com>
2026-05-25 01:44:01 +0200

    [dyad] (2 files changed)

---
+++ api/get.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const m3u = `#EXTM3U
+#EXTINF:-1 tvg-name="Test Channel" group-title="Live TV",Test Channel
+https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8
+`;
+
+  res.setHeader("Content-Type", "application/vnd.apple.mpegurl; charset=utf-8");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+  return res.status(200).send(m3u);
+}
