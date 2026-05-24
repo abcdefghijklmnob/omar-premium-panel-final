e735a401fd486aa8710c888a0a2b8e64af3e8b7d
[dyad] <omarhrytani@gmail.com>
2026-05-25 01:58:39 +0200

    [dyad] (1 files changed)

--- api/get.js
+++ api/get.js
@@ -4,7 +4,13 @@ export default function handler(req, res) {
 https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8
 `;
 
-  res.setHeader("Content-Type", "application/vnd.apple.mpegurl; charset=utf-8");
+  const debug = req?.query?.debug;
+  const isDebug = debug === "1" || debug === 1;
+
+  res.setHeader(
+    "Content-Type",
+    isDebug ? "text/plain; charset=utf-8" : "application/vnd.apple.mpegurl; charset=utf-8",
+  );
   res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
   return res.status(200).send(m3u);
 }
